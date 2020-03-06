8069077b735f68be0da03595b7858a8959e6583c
Mohit Kumar <mk4227525@gmail.com>
2020-03-06 18:58:57 +0530

    trial

---
+++ github-finder/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
