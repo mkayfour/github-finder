25b8d8f2feebd5b2d828a4a0a52515b4f58f3691
Mohit Kumar <mk4227525@gmail.com>
2020-03-09 12:57:44 +0530

    started with user component

---
+++ github-finder/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+
+export class About extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1> About this app </h1>
+        <p>App to search github users</p>
+        <p> version 1.0</p>
+      </Fragment>
+    );
+  }
+}
+
+export default About;
--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = ({ user: { avatar_url, html_url, login } }) => {
   return (
@@ -12,9 +13,9 @@ const UserItem = ({ user: { avatar_url, html_url, login } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
